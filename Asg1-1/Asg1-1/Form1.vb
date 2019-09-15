8aafc447d88b50727b4644c076c251045f80da96
krintyso <54445161+krintyso@users.noreply.github.com>
2019-09-14 22:15:16 -0600

    Assignment
    Assignment

--- Asg1-1/Asg1-1/Form1.vb
+++ Asg1-1/Asg1-1/Form1.vb
@@ -1,15 +1,17 @@
-﻿Public Class Form1
+﻿'By Tyson Kriner
+
+Public Class Form1
 
     Private Sub UpdateLabelHighValyrian_Click(sender As Object, e As EventArgs) Handles UpdateLabelHighValyrian.Click
-        Label1.Text = "Rytsas vys"
+        Label1.Text = "Rytsas vys" 'When the High valyrian button is pressed, hello world is translated in High valyrian
     End Sub
 
     Private Sub UpdateLabelKlingon_Click(sender As Object, e As EventArgs) Handles UpdateLabelKlingon.Click
-        Label1.Text = "qo' vIvan"
+        Label1.Text = "qo' vIvan" 'When the Klingon button is pressed, hello world is translated in Klingon
     End Sub
 
     Private Sub UpdateLabelEnglish_Click(sender As Object, e As EventArgs) Handles UpdateLabelEnglish.Click
-        Label1.Text = "Hello World"
+        Label1.Text = "Hello World" 'When the English button is pressed, hello world is translated in english
     End Sub
 
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
