23790fad1719ed8cca17f47db9ffc39691fd0103
krintyso <54445161+krintyso@users.noreply.github.com>
2019-09-19 21:51:08 -0600

    asg2-6
    asg2-6

--- Asg2-6/Form1.vb
+++ Asg2-6/Form1.vb
@@ -2,7 +2,7 @@
 'RCET0265
 'asg2-6
 'state flag viewer program
-'
+'https://github.com/krintyso/TK-VS-F19/tree/master/Asg2-6
 Public Class Form1
     Private Sub IdahoFlagButton_Click(sender As Object, e As EventArgs) Handles IdahoFlagButton.Click
         FlagDisplay.Image = My.Resources.IdahoFlag
