8f3eb3d368a2b9057422d6b60813bb2e02f13c9c
krintyso <54445161+krintyso@users.noreply.github.com>
2019-12-04 09:09:40 -0700

    sprint2
    sprint 2

---
+++ Sprint2/Form1.vb
@@ -0,0 +1,70 @@
+﻿
+Public Class Form1
+    Private Sub TestValidateAndConvert() Handles Me.Load
+        Dim count As Integer = 0
+        Dim result As Integer = 0
+        Dim pad As Integer = 15
+        Dim report As String = ""
+        Dim temp As String = ""
+        Dim testData = New String(4, 4) {
+    {"5", "2", "17", "8", "42"},
+    {"6.7", "3.14", "5.4", "5.5", "0.125"},
+    {"-21", "-32.1", "-4", "-4.5", "-4.4"},
+    {"", "", "", "", ""},
+    {"True", "False", "lOOlO", "9O2lO", "dog"}}
+        For row = 0 To 4
+            For column = 0 To 4
+                result = 0
+                temp = ValidateAndConvert(testData(row, column), result)
+                report &= ("Trying: " & testData(row, column)).PadRight(pad)
+                If row < 3 Then
+                    If CStr(CInt(testData(row, column))) <> CStr(result) Or temp <> "" Then
+                        report &= " TEST FAIL" & vbNewLine
+                        report &= ("Result is: " & CStr(result)).PadRight(pad) & " : " & temp & vbNewLine
+                        report &= ("Should be: " & CStr(CInt(testData(row, column)))).PadRight(pad) & " : " _
+    & "<Empty>" & vbNewLine
+                    Else
+                        report &= " TEST PASS" & vbNewLine
+                        count += 1
+                    End If
+                ElseIf temp <> "is empty" And row = 3 Then
+                    report &= " TEST FAIL" & vbNewLine
+                    report &= ("Result is: " & CStr(result)).PadRight(pad) & " : " & temp & vbNewLine
+                    report &= ("Should be: " & CStr(0)).PadRight(pad) & " : " & "is empty" & vbNewLine
+                ElseIf temp <> "Must contain a number" And row > 3 Then
+                    report &= " TEST FAIL" & vbNewLine
+                    report &= ("Result is: " & CStr(result)).PadRight(pad) & " : " & temp & vbNewLine
+                    report &= ("Should be: " & CStr(0)).PadRight(pad) & " : " & "Must contain a number" _
+    & vbNewLine
+                Else
+                    report &= " TEST PASS" & vbNewLine
+                    count += 1
+                End If
+            Next
+        Next
+        Console.WriteLine(report & "Passed " & CStr(count) & " of 25 tests. Score: " _
+    & CStr((count / 25) * 100) & "%")
+        MessageBox.Show("Passed " & CStr(count) & " of 25 tests. Score: " _
+    & CStr((count / 25) * 100) & "%")
+    End Sub
+
+    Private Function ValidateAndConvert(ByVal convertThisString, ByRef toThisInteger) As String
+
+
+        Try
+            Decimal.Parse(convertThisString, toThisInteger)
+
+        Catch ex As Exception
+            If convertThisString = "" Then
+                Return "Is empty"
+            Else
+                Return "Must be a number"
+            End If
+
+        End Try
+
+        Return ""
+
+    End Function
+
+End Class
---
+++ Sprint2/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Sprint2")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("Sprint2")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("881e9571-8587-48d1-a6cd-b098a73793e1")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
